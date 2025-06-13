5c58fe70330500ba5411465916bec6721fe982e7
Bernard Banda <bandabernard917@gmail.com>
2025-06-13 11:52:34 +0200

    Added incrementel, decremental and rest functions

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,7 +1,36 @@
+// Importing useState
+import react, { useState } from "react"
 
 const App = () => {
+  // Destructuring initialState and setState from useState()
+  const [initialState, setState] = useState(0);
+
+  // Creating an incremental function
+  const incrementFunc = () => {
+    // setState contains the previous value which is in useState (0) for now
+    setState( (previousValue) => previousValue + 1 );
+  }
+
+  // Creating a decremental function
+  const decrementFunc = () => {
+    // setState contains the previous value which is in useState (0) for now
+    setState( (previousValue) => previousValue - 1 );
+  }
+
+  // Creating a reset function
+  const reserFunc = () => {
+    // setState contains the previous value which is in useState (0) for now
+    setState( (0) );
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1> Set Count: {initialState} </h1>
+
+      <button onClick={incrementFunc}> Increase Count </button>
+      <button onClick={decrementFunc}> Decrease Count </button>
+      <button onClick={reserFunc}> Reset Count </button>
+    </div>
   )
 }
 
